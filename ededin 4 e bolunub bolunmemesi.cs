e11faab16e511978ef150e83003b655cc09e9168
Abbas <97395017+Mammadli999@users.noreply.github.com>
2022-01-21 02:33:08 +0400

    Update ededin 4 e bolunub bolunmemesi.cs

--- ededin 4 e bolunub bolunmemesi.cs
+++ ededin 4 e bolunub bolunmemesi.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
 
-            int a = 83; 
+            int a = 84;
 
             int b = a % 4;
 
@@ -20,6 +20,24 @@ namespace ConsoleApp4
                 Console.WriteLine("Bolunmur");
             }
             Console.ReadKey();
+
+            /*
+            Console.WriteLine("Bir Reqem Yaz");
+
+            int reqem = Convert.ToInt32(Console.ReadLine());
+
+            int a = reqem % 4;
+
+            if (a == 0)                                           // Belede elemey mumkundur
+            {
+                Console.WriteLine("Bolunur");
+            }
+            else
+            {
+                Console.WriteLine("Bolunmur");
+            }
+            Console.ReadKey();
+            */
 
 
 
